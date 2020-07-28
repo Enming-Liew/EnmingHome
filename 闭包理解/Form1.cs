753e362bd7efd6e5c4cb1b0960a5115ee28b6e0a
liutian <820203563@qq.com>
2020-07-28 11:13:46 +0800

    闭包理解

--- 闭包理解/Form1.cs
+++ 闭包理解/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +17,39 @@ namespace 闭包理解
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            usersLoad();
+            string tipWords = "您将关闭当前对话框";
+            this.FormClosing += delegate
+            {
+                MessageBox.Show(tipWords);
+            };
+        }
+
+        private void usersLoad()
+        {
+            List<UserModel> userList = new List<UserModel> {
+                new UserModel { UserName = "liutian", UserAge = 26 },
+                new UserModel { UserName = "Enming", UserAge = 26 },
+                new UserModel { UserName = "Tina", UserAge = 26 }
+            };
+
+            for (int i = 0; i < userList.Count; i++)
+            {
+                UserModel u = userList[i];
+                ThreadPool.QueueUserWorkItem((obj) =>
+                {
+                    Thread.Sleep(1000);
+                    //UserModel u = userList[i];                   
+                    MessageBox.Show(u.UserName);
+                });
+            }
+
+        }
+     
+
+
     }
 }
---
+++ 闭包理解/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 闭包理解
+{
+    public class UserModel
+    {
+        public string UserName
+        {
+            get;
+            set;
+        }
+
+        public int UserAge
+        {
+            get;
+            set;
+        }
+    }
+}
