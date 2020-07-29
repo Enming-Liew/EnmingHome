979969373d686b03979f2a49579766420e7cc82b
Enming-Liew <gitzxmadsb1007>
2020-07-29 10:28:18 +0800

    委托 事件

---
+++ 事件/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 事件
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //加事件用+=   取消事件-=
+            //事件运行的规则和过程与委托一致  执行的类容依然是委托
+            //事件和委托的关联：在定义事件的时候就会有一个委托的定义
+            //并且事件的类型就是当前的委托
+            Hello he = new Hello();
+            China c = new China();
+
+            he.ShowHievent += new Hello.ShowHiDelegate(c.ShowHi);
+            //执行事件
+            he.ClickShowHievent();
+
+        }
+    }
+
+    class Hello
+    {
+        /// <summary>
+        /// 定义事件的委托
+        /// </summary>
+        public delegate void ShowHiDelegate();
+
+        /// <summary>
+        /// 定义你好的事件
+        /// </summary>
+        public event ShowHiDelegate ShowHievent;
+
+        public void ClickShowHievent()
+        {
+            if (ShowHievent != null)
+            {
+                ShowHievent();
+            }
+        }
+    }
+
+
+    class China
+    {
+        public void ShowHi()
+        {
+            Console.WriteLine("中国人说你好");
+            Console.ReadKey();
+        }
+
+    }
+
+
+}
---
+++ 委托/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 委托
+{
+    class Program
+    {
+        //public delegate bool CompareDelegate(Student first, Student second);
+        public static bool Younger(Student s1, Student s2) => s1.Age <= s2.Age;
+
+        public static bool NumSmaller(Student s1, Student s2) => s1.Num <= s2.Num;
+
+        //delegate 委托关键字
+        delegate void ShowHi(string str);
+        static void Main(string[] args)
+        {
+            //定义一个委托并把方法以引用类型的形式加进去
+            ShowHi sh = new ShowHi(ShowHi2);
+            sh("Enming");
+
+            Student s1 = new Student() { Name = "小红", Age = 18, Num = 1001 };
+            Student s2 = new Student() { Name = "小黑", Age = 20, Num = 1000 };
+            List<Student> list = new List<Student>();
+            list.Add(s1);
+            list.Add(s2);
+
+            //Func<Student, Student, bool> c = new Func<Student, Student, bool>(Younger); 等价方法 #
+            //Func<Student, Student, bool> c = Younger;// 等价方法 #
+            //Func<Student, Student, bool> c = delegate (Student s3, Student s4)
+            //  {
+            //      return s3.Num < s4.Num;
+            //  };//匿名委托
+            //Func<Student, Student, bool> c = (s4, s5) => s4.Age <= s5.Age;//Lambda表达式
+            Func<Student, Student, bool> c = (s4, s5) => { return s4.Age <= s5.Age; };
+            SortStudent(list, c);
+
+            //使用委托推断，与上两行等价
+            //SortStudent(list, Younger);
+            Console.ReadKey();
+        }
+
+        public static void SortStudent(List<Student> sList, Func<Student, Student, bool> compareMethod)
+        {
+            //if (compareMethod(sList[0], sList[1]))
+            //{
+
+            //}
+            if (compareMethod.Invoke(sList[0], sList[1]))
+            {
+
+            }
+            else
+            {
+                sList.Reverse();
+            }
+            Console.WriteLine($"\r\n按照 {compareMethod.Method.Name} 排名：");
+            foreach (Student item in sList)
+            {
+                Console.WriteLine($"{item.Name} {item.Age} {item.Num} ");
+            }
+        }
+
+
+
+        //定义一个输出你好的方法
+        static void ShowHi2(string str)
+        {
+            Console.WriteLine("你好,{0}", str);
+
+        }
+
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Num { get; set; }
+    }
+
+}
