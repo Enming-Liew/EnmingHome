3d1881e631d41c5ef97a284b53e191825eafa4da
Enming-Liew <gitzxmadsb1007>
2020-07-31 10:36:58 +0800

    委托高级

--- 委托/Program.cs
+++ 委托/Program.cs
@@ -17,10 +17,13 @@ namespace 委托
         delegate void ShowHi(string str);
         static void Main(string[] args)
         {
+            #region 1.0 委托入门
             //定义一个委托并把方法以引用类型的形式加进去
             ShowHi sh = new ShowHi(ShowHi2);
             sh("Enming");
+            #endregion
 
+            #region 2.0 委托中级
             Student s1 = new Student() { Name = "小红", Age = 18, Num = 1001 };
             Student s2 = new Student() { Name = "小黑", Age = 20, Num = 1000 };
             List<Student> list = new List<Student>();
@@ -38,10 +41,61 @@ namespace 委托
             SortStudent(list, c);
 
             //使用委托推断，与上两行等价
-            //SortStudent(list, Younger);
+            //SortStudent(list, Younger); 
+            #endregion
+
+            #region 3.0 委托高级
+            //3.1委托的用法
+            makeGreet.GreetPeople("EnmingLiew", makeGreet.EnglishGreeting);
+            makeGreet.GreetPeople("刘添", makeGreet.ChineseGreeting);
+            //3.2方法绑定到委托
+            string name1 = "EnmingLiew", name2 = "刘添";
+            makeGreet.GreetPeople(name1, makeGreet.EnglishGreeting);
+            makeGreet.GreetPeople(name2, makeGreet.ChineseGreeting);
+
+            //GreetingDelegate可以像string 一样的赋值
+            //GreetingDelegate delegate1, delegate2;
+            //delegate1 = makeGreet.EnglishGreeting;
+            //delegate2 = makeGreet.ChineseGreeting;
+            //makeGreet.GreetPeople(name1, delegate1);
+            //makeGreet.GreetPeople(name2, delegate2);
+
+            //GreetingDelegate delegate1;
+            //delegate1 = makeGreet.EnglishGreeting;// 先给委托类型的变量赋值
+            //delegate1 += makeGreet.ChineseGreeting; // 给此委托变量再绑定一个方法
+            //// 将先后调用 EnglishGreeting 与 ChineseGreeting 方法
+            //makeGreet.GreetPeople(name1, delegate1);
+
+            //GreetingDelegate delegate1;
+            //delegate1 = makeGreet.EnglishGreeting;
+            //delegate1 += makeGreet.ChineseGreeting;
+            //delegate1(name2);
+
+            GreetingDelegate delegate1 = new GreetingDelegate(makeGreet.EnglishGreeting);
+            delegate1 += makeGreet.ChineseGreeting;
+
+
+            delegate1 -= makeGreet.EnglishGreeting;//取消对EnglishGreeting方法的绑定
+
+            delegate1(name2); 
+            #endregion
+
+
+
+
+
+
+
+
+
             Console.ReadKey();
         }
 
+        /// <summary>
+        /// 学生排序的方法
+        /// </summary>
+        /// <param name="sList"></param>
+        /// <param name="compareMethod"></param>
         public static void SortStudent(List<Student> sList, Func<Student, Student, bool> compareMethod)
         {
             //if (compareMethod(sList[0], sList[1]))
@@ -74,6 +128,10 @@ namespace 委托
 
     }
 
+
+    /// <summary>
+    /// 学生类
+    /// </summary>
     public class Student
     {
         public string Name { get; set; }
---
+++ 委托/makeGreet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 委托
+{
+    public delegate void GreetingDelegate(string name);
+    public static class makeGreet
+    {
+        public static void EnglishGreeting(string name)
+        {
+            Console.WriteLine("\r\nMorning," + name);
+        }
+
+        public static void ChineseGreeting(string name)
+        {
+            Console.WriteLine("\r\n早上好," + name);
+        }
+
+        //注意此方法，它接受一个GreetingDelegate类型的方法作为参数
+        public static void GreetPeople(string name, GreetingDelegate MakeGreeting)
+        {
+            MakeGreeting(name);
+        }
+
+
+    }
+}
